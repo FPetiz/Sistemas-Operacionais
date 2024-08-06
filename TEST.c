36b9969311d34da91a3cd2ebec305a4ee96393ce
Fernanda <adnanref.c.petiz@gmail.com>
2024-08-06 00:59:49 -0300

    comeco do semaforo

--- TEST.c
+++ TEST.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <unistd.h>
-//#include <pthread.h>
-//#include <semaphore.h>
-//#include <sys/syscall.h>
+#include <unistd.h>
+#include <pthread.h>  //está dando algum erro nesses 3 últimos includes
+#include <semaphore.h>
+#include <sys/syscall.h>
 /*#include <time.h> -- para quando formos gerar o tempo de cada um
 ou recebemos o tempo pelo txt também? */
 #define MAX_THREADS 5
@@ -16,11 +16,11 @@ typedef struct Cliente{
 } Cliente;
 
 // Globais
-//sem_t capacetes;
-//sem_t karts;
+sem_t capacetes;
+sem_t karts;
 
-//void* threadC(void* crianca);
-//void* threadA(void* adulto);
+void* threadC(void* crianca);
+void* threadA(void* adulto);
 
 int main() {
 
@@ -34,8 +34,8 @@ int main() {
     Cliente piloto[MAX_THREADS];
     int threads = 0;
 
-    //sem_init(&capacetes, 1, 10);
-    //sem_init(&karts, 1, 10);
+    sem_init(&capacetes, 1, 10);
+    sem_init(&karts, 1, 10);
     
     // Leitura do arquivo e obtenção de dados
     while( threads < MAX_THREADS ) {
@@ -48,43 +48,54 @@ int main() {
         }
     } 
     
-/*
+
     pthread_t tCrianca, tAdulto;
     pthread_create(&tCrianca, NULL, threadC, NULL);
     pthread_create(&tAdulto, NULL, threadA, NULL);
-*/
+
     
     while( threads > 0 ) {
         if ( piloto[threads].idade < 14 && piloto[threads].idade > 6 ) {
             printf("\n%s tem maior prioridade para capacete e beixa para kart.", piloto[threads].nome);
-            //pthread_join(tCrianca, NULL);
+            pthread_join(tCrianca, NULL);
         
         } else if ( piloto[threads].idade > 13 && piloto[threads].idade < 18 ) {
             printf("\n%s tem media prioridade para capacete e baixa para kart.", piloto[threads].nome);
 
         }else {
             printf("\n%s tem baixa prioridade para capacete e alta para kart.", piloto[threads].nome);
-             //pthread_join(tAdulto, NULL);
+            pthread_join(tAdulto, NULL);
         }
         threads--;
     } 
 
-/*  
+ 
     sem_destroy(&capacetes);
     sem_destroy(&karts);
-*/
     
     fclose( file );
     return 0;
 }
 
-/*
+
 void* threadC(void* crianca) {
 
+    sem_wait(&capacetes);
+    sem_wait(&karts);
+
+    //sleep
+
+    sem_post(&karts);
+    sem_post(&capacetes);
 }
 
 
 void* threadA(void* adulto) {
+    sem_wait(&karts);
+    sem_wait(&capacetes);
 
+    //sleep
+
+    sem_post(&capacetes);
+    sem_post(&karts);
 }
-*/
